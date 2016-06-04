4612c172993f86274b148ea88610cef1c483577c
matt182 <mattspring0@gmail.com>
2016-06-04 11:10:20 -0700

    heroku test

--- src/config/DBprepare.php
+++ src/config/DBprepare.php
@@ -24,28 +24,28 @@ try {
     $db->execute();
 
     $conn->exec(
-        "CREATE TABLE IF NOT EXISTS $dbname.members ( id INT NOT NULL SERIAL ,
+        "CREATE TABLE IF NOT EXISTS $dbname.members ( id SERIAL PRIMARY KEY,
 		 					firstName VARCHAR(16) NOT NULL , password VARCHAR(255) NOT NULL ,
 							email VARCHAR(255) NOT NULL , resume` TEXT NULL ,
 							online BOOLEAN NOT NULL DEFAULT FALSE , wasOnline DATETIME NULL ,
 							friends TEXT NOT NULL , reqTo` TEXT NOT NULL , reqFrom TEXT NOT NULL ,
-							PRIMARY KEY (id), UNIQUE (email)) ENGINE = InnoDB
+							UNIQUE (email)) ENGINE = InnoDB
 							 CHARACTER SET utf8 COLLATE utf8_general_ci;"
     );
 
     $conn->exec(
-        "CREATE TABLE IF NOT EXISTS $dbname.blog_records ( id INT NOT NULL SERIAL ,
+        "CREATE TABLE IF NOT EXISTS $dbname.blog_records ( id SERIAL PRIMARY KEY,
 							author_id INT NOT NULL , author_name VARCHAR(16) , owner_id INT NOT NULL , content TEXT NOT NULL ,
 							likes INT NOT NULL DEFAULT '0' , created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-							hasComments BOOLEAN NOT NULL DEFAULT FALSE , PRIMARY KEY (id), INDEX (owner_id))
+							hasComments BOOLEAN NOT NULL DEFAULT FALSE ,INDEX (owner_id))
 							 ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;"
     );
 
     $conn->exec(
-        "CREATE TABLE IF NOT EXISTS $dbname.comments ( id INT NOT NULL SERIAL ,
+        "CREATE TABLE IF NOT EXISTS $dbname.comments ( id SERIAL PRIMARY KEY,
 							record_id INT NOT NULL , `author_id` INT NOT NULL ,
 							author_name VARCHAR(16) NOT NULL , content TEXT NOT NULL ,
-							created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (id), INDEX (record_id))
+							created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,INDEX (record_id))
 							 ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;"
     );
     echo "created";
