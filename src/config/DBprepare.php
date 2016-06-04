0ad456bc5fc60d9ec7c5b6373d0f5c09e8ab72ae
matt182 <mattspring0@gmail.com>
2016-06-04 11:00:10 -0700

    heroku test

--- src/config/DBprepare.php
+++ src/config/DBprepare.php
@@ -24,7 +24,7 @@ try {
     $db->execute();
 
     $conn->exec(
-        "CREATE TABLE IF NOT EXISTS $dbname.`members` ( `id` INT NOT NULL AUTO_INCREMENT ,
+        "CREATE TABLE IF NOT EXISTS $dbname.members ( `id` INT NOT NULL AUTO_INCREMENT ,
 		 					`firstName` VARCHAR(16) NOT NULL , `password` VARCHAR(255) NOT NULL ,
 							`email` VARCHAR(255) NOT NULL , `resume` TEXT NULL ,
 							`online` BOOLEAN NOT NULL DEFAULT FALSE , `wasOnline` DATETIME NULL ,
@@ -34,7 +34,7 @@ try {
     );
 
     $conn->exec(
-        "CREATE TABLE IF NOT EXISTS $dbname.`blog_records` ( `id` INT NOT NULL AUTO_INCREMENT ,
+        "CREATE TABLE IF NOT EXISTS $dbname.blog_records ( `id` INT NOT NULL AUTO_INCREMENT ,
 							`author_id` INT NOT NULL , 'author_name' VARCHAR(16) , `owner_id` INT NOT NULL , `content` TEXT NOT NULL ,
 							`likes` INT NOT NULL DEFAULT '0' , `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
 							`hasComments` BOOLEAN NOT NULL DEFAULT FALSE , PRIMARY KEY (`id`), INDEX (`owner_id`))
@@ -42,12 +42,13 @@ try {
     );
 
     $conn->exec(
-        "CREATE TABLE IF NOT EXISTS $dbname.`comments` ( `id` INT NOT NULL AUTO_INCREMENT ,
+        "CREATE TABLE IF NOT EXISTS $dbname.comments ( `id` INT NOT NULL AUTO_INCREMENT ,
 							`record_id` INT NOT NULL , `author_id` INT NOT NULL ,
 							`author_name` VARCHAR(16) NOT NULL , `content` TEXT NOT NULL ,
 							`created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`), INDEX (`record_id`))
 							 ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;"
     );
+    echo "created";
 } catch(PDOException $e) {
     echo 'unable' . $e->getMessage();
 }
